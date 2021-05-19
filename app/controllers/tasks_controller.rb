c8b7631b12f07007ac87cece54e6dc13cd9976a4
r.m <rnm0427@gmail.com>
2021-05-19 08:12:36 +0000

    review0519 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
-  before_action :correct_user, only: [:show, :destroy, :edit]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :destroy, :edit, :update]
   
   def index
-    if logged_in?
       @tasks = current_user.tasks.order(id: :desc)
-    end
   end
 
   def show
