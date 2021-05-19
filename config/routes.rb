856eb5c282a0a034de66d2137fdb0da55644d9c7
r.m <rnm0427@gmail.com>
2021-05-19 05:22:15 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
   resources :tasks
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:create]
 end
